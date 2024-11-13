6489bf024dd88864cdc9ff11d07a787f6e9c9ac8
hanqjun2660 <hanqjun2660@gmail.com>
2024-11-13 18:07:21 +0900

    header, input 컴포넌트 추가중

---
+++ 05-starting-project/src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./component/Header.jsx";
+import UserInput from "./component/UserInput.jsx";
+import {Fragment} from "react";
+
+function App() {
+  return (
+    <Fragment>
+    <Header />
+    <UserInput />
+    </Fragment>
+  );
+}
+
+export default App
