b00f884a45428de4b69b0d2d22fbf5643d4568b3
hanqjun2660 <hanqjun2660@gmail.com>
2024-11-14 12:05:21 +0900

    input 상태 끌어올리기

--- 05-starting-project/src/App.jsx
+++ 05-starting-project/src/App.jsx
@@ -1,11 +1,32 @@
 import Header from "./component/Header.jsx";
 import UserInput from "./component/UserInput.jsx";
+import {useState} from "react";
+import Results from "./component/Results.jsx";
 
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10
+  });
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput(prevUserInput => {
+      return {
+        // 이전 상태 모두 유지
+        ...prevUserInput,
+        // 새로 들어온 상태만 업데이트
+        [inputIdentifier]: newValue
+      };
+    });
+  }
+
   return (
     <>
-    <Header />
-    <UserInput />
+      <Header />
+      <UserInput userInput={userInput} onChange={handleChange} />
+      <Results input={userInput} />
     </>
   );
 }
--- 05-starting-project/src/component/UserInput.jsx
+++ 05-starting-project/src/component/UserInput.jsx
@@ -1,44 +1,25 @@
-import { useState } from "react";
-
-export default function UserInput() {
-    const [userInput, setUserInput] = useState({
-        initialInvestment: 10000,
-        annualInvestment: 1200,
-        expectedReturn: 6,
-        duration: 10
-    });
-
-    function handleChange(inputIdentifier, newValue) {
-        setUserInput(prevUserInput => {
-            return {
-                // 이전 상태 모두 유지
-                ...prevUserInput,
-                // 새로 들어온 상태만 업데이트
-                [inputIdentifier]: newValue
-            };
-        });
-    }
+export default function UserInput({ onChange, userInput }) {
 
     return(
         <section id="user-input">
             <div className="input-group">
                 <p>
                     <label>Initial Investment</label>
-                    <input type="number" required value={userInput.initialInvestment} onChange={(event) => handleChange('initialInvestment', event.target.value)}/>
+                    <input type="number" required value={userInput.initialInvestment} onChange={(event) => onChange('initialInvestment', event.target.value)}/>
                 </p>
                 <p>
                     <label>Annual Investment</label>
-                    <input type="number" required value={userInput.annualInvestment} onChange={(event) => handleChange('annualInvestment', event.target.value)}/>
+                    <input type="number" required value={userInput.annualInvestment} onChange={(event) => onChange('annualInvestment', event.target.value)}/>
                 </p>
             </div>
             <div className="input-group">
                 <p>
                     <label>Expected Return</label>
-                    <input type="number" required value={userInput.expectedReturn} onChange={(event) => handleChange('expectedReturn', event.target.value)}/>
+                    <input type="number" required value={userInput.expectedReturn} onChange={(event) => onChange('expectedReturn', event.target.value)}/>
                 </p>
                 <p>
                     <label>Duration</label>
-                    <input type="number" required value={userInput.duration} onChange={(event) => handleChange('duration', event.target.value)}/>
+                    <input type="number" required value={userInput.duration} onChange={(event) => onChange('duration', event.target.value)}/>
                 </p>
             </div>
         </section>
