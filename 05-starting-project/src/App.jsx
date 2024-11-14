40eefdc953703cbf336afd886392e404c42f642a
hanqjun2660 <hanqjun2660@gmail.com>
2024-11-14 15:32:40 +0900

    프로젝트 완료

--- 05-starting-project/src/App.jsx
+++ 05-starting-project/src/App.jsx
@@ -11,13 +11,15 @@ function App() {
     duration: 10
   });
 
+  const inputIsValid = userInput.duration >= 1;
+
   function handleChange(inputIdentifier, newValue) {
     setUserInput(prevUserInput => {
       return {
         // 이전 상태 모두 유지
         ...prevUserInput,
         // 새로 들어온 상태만 업데이트
-        [inputIdentifier]: newValue
+        [inputIdentifier]: +newValue  // +를 붙이면 문자열 값을 숫자 값으로 강제함
       };
     });
   }
@@ -26,7 +28,8 @@ function App() {
     <>
       <Header />
       <UserInput userInput={userInput} onChange={handleChange} />
-      <Results input={userInput} />
+      {!inputIsValid && <p className="center">Please enter a duration greater then zero</p>}
+      {inputIsValid && <Results input={userInput} />}
     </>
   );
 }
