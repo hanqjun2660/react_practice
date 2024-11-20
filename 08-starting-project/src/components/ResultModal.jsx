f8f78c498f43349425571c88d325087c3e754463
hanqjun2660 <hanqjun2660@gmail.com>
2024-11-20 17:59:18 +0900

    useImperativeHandle 사용

--- 08-starting-project/src/components/ResultModal.jsx
+++ 08-starting-project/src/components/ResultModal.jsx
@@ -1,9 +1,19 @@
-import { forwardRef } from 'react';     // 다른 컴포넌트의 참조를 전달 받아 사용하고 싶을때 사용
+import { forwardRef, useImperativeHandle, useRef } from 'react';     // forwardRef -> 다른 컴포넌트의 참조를 전달 받아 사용하고 싶을때 사용
 
 // forwardRef 함수로 감싼다.
 const ResultModal = forwardRef(function ResultModal({result, targetTime}, ref) {        // 두번째 인자로 받아야함
+    const dialog = useRef();
+
+    useImperativeHandle(ref, () => {
+        return {
+            open() {
+                dialog.current.showModal();
+            }
+        };
+    });
+
     return(
-        <dialog ref={ref} className="result-modal">
+        <dialog ref={dialog} className="result-modal">
             <h2>You {result}</h2>
             <p>The target time was <strong>{targetTime} seconds.</strong></p>
             <p>You stopped the timer with <strong>X seconds left.</strong></p>
