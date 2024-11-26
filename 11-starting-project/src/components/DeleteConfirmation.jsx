f4b9646a468a064dc870a15f6d29ffe41fb5fa78
hanqjun2660 <hanqjun2660@gmail.com>
2024-11-26 17:27:26 +0900

    useEffect의 의존성을 함수로 설정하였을때 무한루프에 대한 문제 설명 추가

--- 11-starting-project/src/components/DeleteConfirmation.jsx
+++ 11-starting-project/src/components/DeleteConfirmation.jsx
@@ -12,7 +12,9 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
             console.log('Clear timer');
             clearTimeout(timer);
         }
-    }, []);
+        // onConfirm 함수는 APP 컴포넌트에 존재한다. App 컴포넌트의 상태가 변경되어 재로딩되면 그 컴포넌트 내 함수는 재생성된다. 고로 여기서 의존성으로 설정하면 무한루프의 위험이 있다.
+        // 하지만 현재 어플리케이션에서는 Modal 컴포넌트에서 open props를 이용해 조건적으로 컴포넌트를 삭제하므로 무한루프에 빠지지는 않는다.
+    }, [onConfirm]);
 
   return (
     <div id="delete-confirmation">
