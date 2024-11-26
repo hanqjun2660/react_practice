9f74d851f8b62e7cc411c696e1a655048b42b144
hanqjun2660 <hanqjun2660@gmail.com>
2024-11-26 14:25:10 +0900

    useEffect 사용, useEffect가 필요없는 경우 추가

---
+++ 11-starting-project/src/App.jsx
@@ -0,0 +1,101 @@
+import { useRef, useState, useEffect } from 'react';
+
+import Places from './components/Places.jsx';
+import { AVAILABLE_PLACES } from './data.js';
+import Modal from './components/Modal.jsx';
+import DeleteConfirmation from './components/DeleteConfirmation.jsx';
+import logoImg from './assets/logo.png';
+import {sortPlacesByDistance} from "./loc.js";
+
+function App() {
+  const modal = useRef();
+  const selectedPlace = useRef();
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [pickedPlaces, setPickedPlaces] = useState([]);
+
+  /*
+    useEffect는 다른 React Hook과 다르게 값을 반환하지 않음.
+    대신 부수효과(side effect) 코드를 첫번째 인자, 두번째 인자는 의존성 배열을 넣어줘야 함.
+    실행 시점은 컴포넌트 함수가 실행 완료후 실행되기 때문에 즉시 실행되지 않음. 즉 무한루프를 해결할 수 있음
+   */
+  useEffect(() => {
+    // 위치를 가져오기 위한 함수(해당 함수는 콜백 함수를 필요로 함)
+    navigator.geolocation.getCurrentPosition((position) => {
+      const sortedPlaces = sortPlacesByDistance(AVAILABLE_PLACES, position.coords.latitude, position.coords.longitude);
+      setAvailablePlaces(sortedPlaces);
+    });
+  }, []);   // 의존성 값을 보고 Effect 함수를 재실행할것인지 판단함.
+
+
+  function handleStartRemovePlace(id) {
+    modal.current.open();
+    selectedPlace.current = id;
+  }
+
+  function handleStopRemovePlace() {
+    modal.current.close();
+  }
+
+  function handleSelectPlace(id) {
+    setPickedPlaces((prevPickedPlaces) => {
+      if (prevPickedPlaces.some((place) => place.id === id)) {
+        return prevPickedPlaces;
+      }
+      const place = AVAILABLE_PLACES.find((place) => place.id === id);
+      return [place, ...prevPickedPlaces];
+    });
+
+    /*
+      앱을 다시 로딩했을 때 선택한 장소들의 상태가 그대로 유지되게 해봄(로컬 스토리지 사용)
+      리액트 앱의 랜더링과 상관없는 부수효과 코드이지만 상태를 변경하지 않는다.
+      이런경우 useEffect를 사용하지 않아도 된다.(그리고 해당 코드는 함수내 있기 때문에 useEffect 사용시 hook 규칙을 위반한다.)
+     */
+    const storedIds = JSON.stringify(localStorage.getItem('seletedPlaces')) || [];
+    if(storedIds.indexOf(id) === -1) {
+      localStorage.setItem('selectedPlaces', JSON.stringify([id, ...storedIds]));
+    }
+  }
+
+  function handleRemovePlace() {
+    setPickedPlaces((prevPickedPlaces) =>
+      prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
+    );
+    modal.current.close();
+  }
+
+  return (
+    <>
+      <Modal ref={modal}>
+        <DeleteConfirmation
+          onCancel={handleStopRemovePlace}
+          onConfirm={handleRemovePlace}
+        />
+      </Modal>
+
+      <header>
+        <img src={logoImg} alt="Stylized globe" />
+        <h1>PlacePicker</h1>
+        <p>
+          Create your personal collection of places you would like to visit or
+          you have visited.
+        </p>
+      </header>
+      <main>
+        <Places
+          title="I'd like to visit ..."
+          fallbackText={'Select the places you would like to visit below.'}
+          places={pickedPlaces}
+          onSelectPlace={handleStartRemovePlace}
+        />
+        <Places
+          title="Available Places"
+          places={availablePlaces}
+          fallbackText="Sorting places by distance..."
+          onSelectPlace={handleSelectPlace}
+        />
+      </main>
+    </>
+  );
+}
+
+export default App;
