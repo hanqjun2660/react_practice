b1946c1ca66f74aac5760c4a62f3ba65bf51661a
hanqjun2660 <hanqjun2660@gmail.com>
2024-11-28 11:54:18 +0900

    동일 레밸의 컴포넌트에서 동일 상태를 key로 활용함에 따른 에러 해결을 위해 컴포넌트로 묶어 동일키 사용

---
+++ 12-starting-project/src/components/Answers.jsx
@@ -0,0 +1,47 @@
+import QUESTIONS from "../questions.js";
+import {useRef} from "react";
+
+export default function Answers({answers, selectedAnswer, answerState, onSelect}) {
+    /**
+     * userAnswer 상태 변경 추가에 따라 답변 선택시 suffle 함수가 재실행되는 문제가 발생.
+     * useState, useEffect를 최소화 하기 위해 useRef 방식으로 수정 -> 참조 방식으로만 현재 컴포넌트에서 진행했을때 또 다른 문제 발생
+     * 답변 목록이 1회만 섞이고 다음 문제에서부터 섞이지 않음. 해결을 위해 답변 컴포넌트로 분리 진행
+     */
+    const shuffledAnswers = useRef();
+
+    function shuffle(answers) {
+        for(let i = answers.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [answers[i], answers[j]] = [answers[j], answers[i]];
+        }
+        return answers;
+    }
+
+    // useRef 방식으로 변경되었으니 suffledAnswers 설정을 .current를 이용해 접근하여 설정
+    if(!shuffledAnswers.current) {
+        shuffledAnswers.current = shuffle([...answers]);
+    }
+
+    return (
+        <ul id="answers">
+            {shuffledAnswers.current.map(answer => {
+                const isSelected = selectedAnswer === answer;
+                let cssClasses = '';
+
+                if(answerState === 'answerd' && isSelected) {
+                    cssClasses = 'selected';
+                }
+
+                if((answerState === 'correct' || answerState === 'wrong') && isSelected) {
+                    cssClasses = answerState;
+                }
+
+                return(
+                    <li key={answer} className="answer">
+                        <button className={cssClasses} onClick={() => onSelect(answer)}>{answer}</button>
+                    </li>
+                );
+            })}
+        </ul>
+    );
+}
--- 12-starting-project/src/components/Quiz.jsx
+++ 12-starting-project/src/components/Quiz.jsx
@@ -2,30 +2,43 @@ import {useState, useCallback} from "react";
 import QUESTIONS from "../questions.js";
 import completeImg from "../assets/quiz-complete.png";
 import QuestionTimer from "./QuestionTimer.jsx";
+import Answers from "./Answers.jsx";
+import Question from "./Question.jsx";
 
 export default function Quiz() {
+
+    const [answerState, setAnswerState] = useState('');
     // 사용자가 선택한 답을 등록하기 위해 관리
     const [userAnswers, setUserAnswers] = useState([]);
     // 사용자가 선택한 답을 저장한 길이의 배열만큼의 인덱스를 저장하면 해당하는 질문 인덱스와 동일해진다.
-    const activeQuestionIndex = userAnswers.length;
+    const activeQuestionIndex = answerState === '' ? userAnswers.length : userAnswers.length - 1;
 
     const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
-    function shuffle(answers) {
-        for(let i = answers.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [answers[i], answers[j]] = [answers[j], answers[i]];
-        }
-        return answers;
-    }
-
     // 이 함수는 원래 컴포넌트 단에서 생성된 함수로 컴포넌트 상태에 의존되어 재생성된다. 그러니 전달한 컴포넌트도 이에 대해 영향을 받아 재평가 되면서 useEffect로 처리해도 새로운 함수로 판단하여 timeout이 재생성되었다. 이를 해결햐기 위해 useCallback을 사용.
     const handleSelectAnswer = useCallback(function handleSelectAnswer(selectedAnswer) {
+
+        // 답변을 선택했다는 상태를 저장.
+        setAnswerState('answerd');
+
         // 이전에 유저가 선택한 답안을 유지하면서 업데이트
         setUserAnswers((prevUserAnswers) => {
             return [...prevUserAnswers, selectedAnswer];
         });
-    }, []);
+
+        // 1초뒤 정답과 선택된 답안을 비교하여 정답 여부를 업데이트
+        setTimeout(() => {
+            if(QUESTIONS[activeQuestionIndex].answers[0] === selectedAnswer) {
+                setAnswerState('correct');
+            } else {
+                setAnswerState('wrong');
+            }
+
+            setTimeout(() => {
+                setAnswerState('');
+            }, 2000)
+        },1000);
+    }, [activeQuestionIndex]);      // activeQuestionIndex를 사용하고 activeQuestionIndex가 업데이트 될때마다 재생성되어야 하니 의존성 배열에 추가
 
     // 위와 똑같이 useCallback을 사용해 의존성으로 handleSelectAnswer로 설정하여 상수에 저장하여 하위 컴포넌트로 전달하였다.
     const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer])
@@ -38,21 +51,9 @@ export default function Quiz() {
         </div>
     )}
 
-    const shuffledAnswers = shuffle([...QUESTIONS[activeQuestionIndex].answers]);
-
     return (
         <div id="quiz">
-            <div id="question">
-                <QuestionTimer timeout={10000} onTimeout={handleSkipAnswer} key={activeQuestionIndex} />
-                <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
-                <ul id="answers">
-                    {shuffledAnswers.map(answer => (
-                        <li key={answer} className="answer">
-                            <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
-                        </li>
-                    ))}
-                </ul>
-            </div>
+            <Question key={activeQuestionIndex} questionText={QUESTIONS[activeQuestionIndex].text} answers={QUESTIONS[activeQuestionIndex].answers} onSelectAnswer={handleSelectAnswer} selectedAnswer={userAnswers[userAnswers.length - 1]} answerState={answerState} onSkipAnswer={handleSkipAnswer}/>
         </div>
     );
 }
