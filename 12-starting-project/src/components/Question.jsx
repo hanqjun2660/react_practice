806c333752ddd7ef86fb4e2553c6458439aec520
hanqjun2660 <hanqjun2660@gmail.com>
2024-11-28 15:32:52 +0900

    타이머 컴포넌트 수정

--- 12-starting-project/src/components/Question.jsx
+++ 12-starting-project/src/components/Question.jsx
@@ -4,7 +4,6 @@ import QUESTIONS from "../questions.js";
 import {useState} from "react";
 
 export default function Question({index, onSelectAnswer, onSkipAnswer}) {
-
     /**
      * Quiz 컴포넌트에서 관리하던 답변 상태를 해당 컴포넌트를 생성하면서 Quiz 컴포넌트에서 관리할 필요가 없어짐.
      * 해당 컴포넌트에서 관리하도록 추가.
@@ -13,6 +12,16 @@ export default function Question({index, onSelectAnswer, onSkipAnswer}) {
        selectedAnswer: '',
        isCorrect: null
     });
+
+    let timer = 10000;
+
+    if(answer.selectedAnswer) {
+        timer = 1000;
+    }
+
+    if(answer.isCorrect !== null) {
+        timer = 2000;
+    }
 
     function handleSelectAnswer(answer) {
         setAnswer({
@@ -23,7 +32,7 @@ export default function Question({index, onSelectAnswer, onSkipAnswer}) {
         setTimeout(() => {
             setAnswer({
                 selectedAnswer: answer,
-                isCorrect: QUESTIONS[index].answers[0] === answer ? true : false
+                isCorrect: QUESTIONS[index].answers[0] === answer
             })
 
             setTimeout(() => {
@@ -34,7 +43,7 @@ export default function Question({index, onSelectAnswer, onSkipAnswer}) {
 
     let answerState = '';
 
-    if(answer.selectedAnswer) {
+    if(answer.selectedAnswer && answer.isCorrect !== null) {
         answerState = answer.isCorrect ? 'correct' : 'wrong';
     } else if(answer.selectedAnswer) {
         answerState = 'answered';
@@ -42,7 +51,7 @@ export default function Question({index, onSelectAnswer, onSkipAnswer}) {
 
     return (
         <div id="question">
-            <QuestionTimer timeout={10000} onTimeout={onSkipAnswer} />
+            <QuestionTimer key={timer} timeout={timer} onTimeout={answer.selectedAnswer === '' ? onSkipAnswer : null} mode={answerState} />
             <h2>{QUESTIONS[index].text}</h2>
             <Answers answers={QUESTIONS[index].answers} selectedAnswer={answer.selectedAnswer} answerState={answerState} onSelect={handleSelectAnswer}/>
         </div>
