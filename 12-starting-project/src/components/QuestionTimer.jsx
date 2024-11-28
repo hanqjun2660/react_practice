ab905aebcbe364436211c0150ac0f555948c52c9
hanqjun2660 <hanqjun2660@gmail.com>
2024-11-28 09:50:41 +0900

    타이머 cleanup 추가, 타이머 컴포넌트 재생성을 위한 key props 설정

--- 12-starting-project/src/components/QuestionTimer.jsx
+++ 12-starting-project/src/components/QuestionTimer.jsx
@@ -5,14 +5,22 @@ export default function QuestionTimer({ timeout, onTimeout }) {
 
     useEffect(() => {
         console.log('SETTING TIMEOUT');
-        setTimeout(onTimeout, timeout);
+        const timer = setTimeout(onTimeout, timeout);
+        return () => {
+            clearTimeout(timer);
+        };
     }, [onTimeout, timeout]);       // 상위 컴포넌트에서 전달받은 onTimeout이 컴포넌트 상태에 따라 재설정되어 여기서도 영향을 받아 해당 useEffect가 영향을 받았었지만 상위에서 useCallback으로 해결하였다.
 
+    // Strict Mdoe로 인해 현재 Effect 함수가 두번 실행됨. 설정한 시간보다 빨리 사라짐. 문제를 해결하기위해서 cleanup이 필요함.
     useEffect(() => {
         console.log('SETTING INTERVAL');
-        setInterval(() => {
+        const interval = setInterval(() => {
             setRemainingTime(prevRemainingTime => prevRemainingTime - 100);
         }, 100);
+
+        return () => {
+            clearInterval(interval);    // cleanup
+        };
     }, []);
 
     return (
