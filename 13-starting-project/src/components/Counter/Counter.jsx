5a29873ae531333ae2570fa59aa60ce791ba45a2
hanqjun2600 <hanqjun2660@gmail.com>
2024-11-29 11:32:19 +0900

    memo추가 후에도 컴포넌트 재실행 되는 문제를 해결하기 위해 useCallback을 추가

--- 13-starting-project/src/components/Counter/Counter.jsx
+++ 13-starting-project/src/components/Counter/Counter.jsx
@@ -1,4 +1,4 @@
-import { useState, memo } from 'react';
+import { useState, memo, useCallback } from 'react';
 
 import IconButton from '../UI/IconButton.jsx';
 import MinusIcon from '../UI/Icons/MinusIcon.jsx';
@@ -37,13 +37,18 @@ const Counter = memo(function Counter({ initialCount }) {
 
   const [counter, setCounter] = useState(initialCount);
 
-  function handleDecrement() {
+  /**
+   * IconButton 컴포넌트에서 memo를 사용했음에도 하위 컴포넌트들이 재실행되는 문제가 발생
+   * 이를 해결하기 위해 props로 전달하는 함수가 해당 컴포넌트에서 생성되기 때문에 새로운 props로 인식하는걸 알았음.
+   * useCallback 훅을 사용하여 재실행되지 않게 처리하여 전달함.
+   */
+  const handleDecrement = useCallback(function handleDecrement() {
     setCounter((prevCounter) => prevCounter - 1);
-  }
+  }, []);
 
-  function handleIncrement() {
+  const handleIncrement = useCallback(function handleIncrement() {
     setCounter((prevCounter) => prevCounter + 1);
-  }
+  }, []);
 
   return (
     <section className="counter">
