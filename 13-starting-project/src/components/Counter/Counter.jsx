49932f12fc1f25bc9cca342c6fb33a599b2e039d
hanqjun2600 <hanqjun2660@gmail.com>
2024-11-29 15:40:37 +0900

    millon.js 추가

--- 13-starting-project/src/components/Counter/Counter.jsx
+++ 13-starting-project/src/components/Counter/Counter.jsx
@@ -1,10 +1,11 @@
-import { useState, memo, useCallback, useMemo } from 'react';
+import {useState, memo, useCallback, useMemo} from 'react';
 
 import IconButton from '../UI/IconButton.jsx';
 import MinusIcon from '../UI/Icons/MinusIcon.jsx';
 import PlusIcon from '../UI/Icons/PlusIcon.jsx';
 import CounterOutput from './CounterOutput.jsx';
 import { log } from '../../log.js';
+import CounterHistory from "./CounterHistory.jsx";
 
 function isPrime(number) {
   log(
@@ -42,7 +43,18 @@ const Counter = memo(function Counter({ initialCount }) {
    */
   const initialCountIsPrime = useMemo(() => isPrime(initialCount), [initialCount]);
 
-  const [counter, setCounter] = useState(initialCount);
+  /*useEffect(() => {
+    setCounterChanges([
+      {value : initialCount, id: Math.random() * 1000 }
+    ]);
+  }, [initialCount]);*/
+
+  // const [counter, setCounter] = useState(initialCount);
+  const [counterChanges, setCounterChanges] = useState([{value: initialCount, id: Math.random() * 1000}]);
+
+  const currentCounter = counterChanges.reduce(
+      (prevCounter, counterChange) => prevCounter + counterChange.value, 0
+  )
 
   /**
    * IconButton 컴포넌트에서 memo를 사용했음에도 하위 컴포넌트들이 재실행되는 문제가 발생
@@ -50,11 +62,13 @@ const Counter = memo(function Counter({ initialCount }) {
    * useCallback 훅을 사용하여 재실행되지 않게 처리하여 전달함.
    */
   const handleDecrement = useCallback(function handleDecrement() {
-    setCounter((prevCounter) => prevCounter - 1);
+    // setCounter((prevCounter) => prevCounter - 1);
+    setCounterChanges((prevCounterChanges) => [{value: -1, id: Math.random() * 1000 }, ...prevCounterChanges]);
   }, []);
 
   const handleIncrement = useCallback(function handleIncrement() {
-    setCounter((prevCounter) => prevCounter + 1);
+    // setCounter((prevCounter) => prevCounter + 1);
+    setCounterChanges((prevCounterChanges) => [{value: 1, id: Math.random() * 1000 }, ...prevCounterChanges]);
   }, []);
 
   return (
@@ -67,11 +81,12 @@ const Counter = memo(function Counter({ initialCount }) {
         <IconButton icon={MinusIcon} onClick={handleDecrement}>
           Decrement
         </IconButton>
-        <CounterOutput value={counter} />
+        <CounterOutput value={currentCounter} />
         <IconButton icon={PlusIcon} onClick={handleIncrement}>
           Increment
         </IconButton>
       </p>
+      <CounterHistory history={counterChanges}/>
     </section>
   );
 });
